f95a9bf1a034e6510554785a6636f623d69bf72f
Naresh <naresh.bhadu@olx.in>
2025-07-24 15:08:22 +0530

    initial setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hekko")
+from fastapi import FastAPI
+from src.api import app , health
+import uvicorn
+
+server = FastAPI(title="AI-Chatbot", version="0.1.0")
+
+server.include_router(app.router)
+server.include_router(health.router, tags=["Health"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:server", host="0.0.0.0", port=8000, reload=True)
